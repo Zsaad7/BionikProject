209af302bb5103e567b689ab6bba3e649797bc4b
Saad Zridi <30910267+Zsaad7@users.noreply.github.com>
2020-03-19 13:03:27 +0100

    Modif 1 : tsawer
    Tsawer dial home, hzit trois mn Bionik.fr, u drthom nit huma f site hta nchufo kindiro lihom nakhdo les tofs mn eando ola chi haja komme ca ! db anchuf routing issue

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -122,3 +122,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
     <!-- Header Info End -->
     <!-- Header End -->
+     <!-- Hero Slider Begin -->
+    <section class="hero-slider">
+        <div class="hero-items owl-carousel">
+            <div class="single-slider-item set-bg" data-setbg="<?php echo IMG ?>/NavigationPhotos/1.jpg ">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>2019</h1>
+                            <h2>Lookbook.</h2>
+                            <a href="#" class="primary-btn">See More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slider-item set-bg" data-setbg="<?php echo IMG ?>/NavigationPhotos/2.jpg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>2019</h1>
+                            <h2>Lookbook.</h2>
+                            <a href="#" class="primary-btn">See More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-slider-item set-bg" data-setbg="<?php echo IMG ?>/NavigationPhotos/3.jpg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>2019</h1>
+                            <h2>Lookbook.</h2>
+                            <a href="#" class="primary-btn">See More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Slider End -->
