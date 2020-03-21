2a7c7696d9608aa27e5bf367e4683208938d881c
Saad Zridi <saadzridi25@gmail.com>
2020-03-21 13:39:22 +0100

    begin tache Gamme

--- application/views/gamme_view.php
+++ application/views/gamme_view.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  <!-- Latest Product Begin -->
     <section class="latest-products spad">
         <div class="container">
@@ -118,66 +118,4 @@
         </div>
     </section>
     <!-- Latest Product End -->
-=======
-<h1>Hello Test GitKraken</h1>
-<h1>sipiu
-</h1>
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
 
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-
-
-<h1>sipiu
-</h1>
-
-<h1>sipiu
-</h1>
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-
-
-<h1>sipiu
-</h1>
-
-
-<h1>sipiu
-</h1>
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-<p>aodpufvapfvhinoavadfvgpvasduipvop=]08ytrtiop[poiuygfddddddddddddddddcv d ,]sad-0cpd WECB8we7cnwaep[cwep8dgw\p[ weWE0-</p>
-
-
-
-
-<h1>sipiu
-</h1>
->>>>>>> master
